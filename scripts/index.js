574a6bdaf33191e20790b0548d01fcb0bcdbb38c
Lucas Hélio <heliobsb.almeida@icloud.com>
2024-08-31 13:27:55 -0300

    feat: construção da página e adição de conteúdo

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+(() => {
+    function atualizaAno() {
+        const anoEl = document.getElementById('ano');
+        if (!!anoEl) {
+            anoEl.innerText = (new Date()).getFullYear() + '';
+        }
+    }
+
+    atualizaAno();
+})();
+
+function atualizaModal(json) {
+    document.getElementById('titulo-modal').innerHTML = json.title;
+    document.getElementById('conteudo-modal').innerHTML = json.description;
+}
+
+function abrirModal(personagem) {
+    fetch('/assets/mocks/personagens.json')
+        .then(res => res.json())
+        .then((json) => {
+            console.log(json[personagem]);
+            atualizaModal(json[personagem])
+        });
+}
